068633957ae40765d839a3f5e0f41700d9c74b0d
nilarnab mookherjee <niluriju@gmail.com>
2019-04-02 12:24:03 +0530

    initial learning and experimentations

--- src/index.js
+++ src/index.js
@@ -1,34 +1,68 @@
 import Raphael from './raphael/raphael';
 
 class Node {
-  constructor(label) {
+  constructor(label, x, y) {
     this.label = label;
+    this.x = x;
+    this.y = y;
     this.children = [];
   }
 
-  addChild(node) {
-    this.node.push(node);
+  addChild (node) {
+    this.children.push(node);
   }
 }
 
-const root = new Node("Root");
-root.addChild(new Node('child-1'));
-root.addChild(new Node('child-2'));
-root.addChild(new Node('child-3'));
-root.addChild(new Node('child-4'));
-root.addChild(new Node('child-5'));
-root.addChild(new Node('child-6'));
-root.addChild(new Node('child-7'));
-
-const tree =  [
-  root  
+const root = new Node("Root", 100, 100);
+root.addChild(new Node('child-1', 120, 200));
+root.addChild(new Node('child-2', 120, 300));
+root.addChild(new Node('child-3', 120, 400));
+root.addChild(new Node('child-4', 120, 500));
+root.addChild(new Node('child-5', 120, 600));
+root.addChild(new Node('child-6', 120, 700));
+root.addChild(new Node('child-7', 120, 800));
+const root2 = new Node("Root-2", 100, 900);
+const PIN_RADIUS = 10;
+const tree = [
+  root,
+  root2,
 ];
+const p = Raphael(100, 100, 1000, 1000);
 
-function drawTree(tree) {
-  const p = Raphael(0,0, 400, 400);
+function drawTree (tree) {
   tree.forEach(node => {
-    p.circle(50, 50, 20);
+    const { x, y } = node;
+
+    const path = p.path(["M", x, y, "C", x, y, x, y + 100, x + 20, y + 100]);
+    resetPathHighlight(path);
+
+    const pin = p.circle(x, y, PIN_RADIUS);
+    pin.attr({
+      fill: "rgb(75,75,75)"
+    });
+
+    path.mouseover(() => { highlightPath(path); });
+    pin.mouseover(() => { highlightPath(path); });
+    pin.mouseout(() => { resetPathHighlight(path); });
+    path.mouseout(() => { resetPathHighlight(path); });
+
+    if (node.children.length > 0)
+      drawTree(node.children);
   });
+
+  function highlightPath (path) {
+    path.attr({
+      'stroke-width': 6,
+      'stroke': '#ff0000'
+    });
+  }
+
+  function resetPathHighlight (path) {
+    path.attr({
+      'stroke-width': 4,
+      'stroke': '#00ff00'
+    });
+  }
 }
-console.log("Hi");
+
 drawTree(tree);
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,25 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js'],
+  devtool: 'inline-source-map',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
+    publicPath: '/dist',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
